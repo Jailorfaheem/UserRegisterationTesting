db5b8a9828067ac9e542e600e216fdf7422a6397
faheem <lalafahim55@gmail.com>
2021-12-07 20:36:12 +0530

    Faheem UC1 for first name testing

---
+++ UserRegisterationTesting/TestClass/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegularExpression00;
+
+namespace ExceptionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidFirstNameOfUser()
+        {
+            //AAA method
+            //Arrange
+            string name = "Faheem";
+            string expected = "invalid";
+            //creating object of moodanalyzer class and passing message
+            //RegularExpression regularExpression = new RegularExpression(name);
+
+            //Act
+            string actual = RegularExpression.FirstNameValid(name);
+
+            //Assert
+            //comparing actual and expected value
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
